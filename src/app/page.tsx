8bd6df5c090ca2cdecff6ef447c81fb50fba6838
tommyhu234 <tommyhu234@gmail.com>
2024-01-27 15:52:10 +0100

    Fixed build fail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,13 @@
 'use client'
 
-import { KeyboardEvent, useCallback } from "react"
+import { KeyboardEvent } from "react"
 import { navigate } from "./navigate"
-import { useSearchParams } from "next/navigation"
 
 export default function Home() {
-  const searchParams = useSearchParams()
-
-  // Get a new searchParams string by merging the current
-  // searchParams with a provided key/value pair
-  const createQueryString = useCallback(
-    (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams.toString())
-      params.set(name, value)
-
-      return params.toString()
-    },
-    [searchParams]
-  )
 
   const keyDown = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
-      navigate('/profile' + '?' + createQueryString('username', event.currentTarget.value))
+      navigate(`/profile?username=${event.currentTarget.value}`)
     }
   }
 
