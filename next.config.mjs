a1488f75157dcf34b4cea29f0e6287f2fae2665f
tommyhu234 <tommyhu234@gmail.com>
2024-03-04 21:36:38 +0100

    Fixed dirname

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const nextConfig = {
   env: {
     PROJECT_ROOT: __dirname
