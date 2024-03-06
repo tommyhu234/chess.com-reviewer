24beb1d2afe4f5f93e3ce743695945bf5c73f0a7
tommyhu234 <tommyhu234@gmail.com>
2024-03-06 16:45:19 +0100

    Added symbolds for blunders and mistakes

--- src/app/analysis/analysis_move.tsx
+++ src/app/analysis/analysis_move.tsx
@@ -1,11 +1,26 @@
 'use client'
 
 import { Move } from "chess.js"
+import Image from 'next/image'
 
 const getMoveColor = (moveType: string) => {
-  if (moveType === "Blunder") return "text-red-500"
-  if (moveType === "Mistake") return "text-yellow-500"
+  if (moveType === "Blunder") return "text-[#FA412D]"
+  if (moveType === "Mistake") return "text-[#FFA459]"
   else return "text-gray-light"
+}
+
+const getMoveSymbol = (moveType: string) => {
+  if (moveType === "Blunder") {
+    return <div className="mr-[5px]">
+      <Image src="/moveTypes/blunder.svg" width={16} height={16} alt="" />
+    </div>
+  } else if (moveType === "Mistake") {
+    return <div className="mr-[5px]">
+      <Image src="/moveTypes/mistake.svg" width={16} height={16} alt="" />
+    </div>
+  } else {
+    return <div className="w-[16px] h-[16px] mr-[5px]" />
+  }
 }
 
 export default function AnalysisMove({ position, setPosition, move, index, evaluation }: { position: any, setPosition: any, move: Move[], index: number, evaluation: any }) {
@@ -21,21 +36,23 @@ export default function AnalysisMove({ position, setPosition, move, index, evalu
     <div
       key={`${index}-${move[0].san}-${move[1] ? move[1].san : ""}`}
       className={`flex px-3 h-[30px] items-center cursor-pointer ${position.key.split("-")[0] == index ? "bg-main" : index % 2 === 0 ? "bg-secondary" : "bg-[#454441]"}`}>
-      <div className="text-gray font-semibold w-[3rem]">{index}.</div>
-      <div className={`${getMoveColor(evaluation[0].moveType)} hover:text-white font-semibold w-[5rem]`}>
+      <div className="text-gray font-semibold w-[20px] mr-[10px]">{index}.</div>
+      <div className={`${getMoveColor(evaluation[0].moveType)} flex items-center hover:text-white font-semibold w-[74px] mr-[10px]`}>
+        {getMoveSymbol(evaluation[0].moveType)}
         <div className={`w-[49px] rounded-[3px] p-1 ${position.key === `${index}-${move[0].san}` ? "bg-[#5A5956]" : ""}`} onClick={() => handleMoveClick(move[0])}>
           {move[0].san}
         </div>
       </div>
       {
         move[1] ? (
-          <div className={`${getMoveColor(evaluation[1].moveType)} text-gray-light hover:text-white font-semibold w-[5rem]`}>
+          <div className={`${getMoveColor(evaluation[1].moveType)} flex items-center hover:text-white font-semibold w-[74px] mr-[20px]`}>
+            {getMoveSymbol(evaluation[1].moveType)}
             <div className={`w-[49px] rounded-[3px] p-1 ${position.key === `${index}-${move[1].san}` ? "bg-[#5A5956]" : ""}`} onClick={() => handleMoveClick(move[1])}>
               {move[1].san}
             </div>
           </div>
         ) : (
-          <div className="w-[5rem]" />
+          <div className="w-[74px] mr-[20px]" />
         )
       }
     </div>
