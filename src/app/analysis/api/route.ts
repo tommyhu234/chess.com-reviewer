b865c4934d81eeec3e9fffd72b90711a39c54db7
tommyhu234 <tommyhu234@gmail.com>
2024-05-06 15:45:19 +0200

    Improved stockfish moves parsing

--- src/app/analysis/api/route.ts
+++ src/app/analysis/api/route.ts
@@ -42,85 +42,21 @@ export async function POST(request: Request) {
   const chess = new Chess()
   chess.loadPgn(body)
   const moves = chess.history({ verbose: true })
+  const depth = 15
 
   const getEvaluations: Promise<Evaluation[][]> = new Promise((resolve) => {
     const evaluations: Evaluation[] = []
-    let bestMovesCount = 0
+    const stockfishPath = "stockfish-windows-x86-64-avx2/stockfish/stockfish-windows-x86-64-avx2.exe"
+    const now = Date.now()
+    const stockfish = spawn(stockfishPath)
+    let moveIndex = 0
 
-    for (let i = 0; i <= moves.length; i++) {
-      let stockfishPath
-      if (process.env.NODE_ENV === "development") {
-        stockfishPath = "stockfish-windows-x86-64-avx2/stockfish/stockfish-windows-x86-64-avx2.exe"
-      } else stockfishPath = "stockfish-ubuntu-x86-64/stockfish/src/stockfish"
-      const stockfish = spawn(stockfishPath)
-
-      const now = Date.now()
-
-      stockfish.stdout.on('data', (data) => {
-        if (data.includes("bestmove")) {
-          const dataArray = data.toString().split(" ")
-          const bestMove = dataArray[dataArray.findIndex((x: string) => x.includes("bestmove")) + 1].substring(0, 4)
-          const isWhite = i % 2 !== 1
-          if (evaluations[i]) {
-            evaluations[i].bestMove = bestMove
-          } else {
-            if (dataArray.includes("score")) {
-              let bestScore
-              if (dataArray.includes("mate")) {
-                const value = dataArray[dataArray.findIndex((x: string) => x.includes("mate")) + 1]
-                if (value.startsWith("-")) {
-                  if (isWhite) bestScore = `-M${value.substring(1, value.length)}`
-                  else bestScore = `M${value.substring(1, value.length)}`
-                }
-                else {
-                  if (isWhite) bestScore = `M${value}`
-                  else bestScore = `-M${value}`
-                }
-              } else bestScore = (parseInt(dataArray[dataArray.findIndex((x: string) => x.includes("cp")) + 1]) / (isWhite ? 100 : -100)).toString()
-              evaluations[i] = {
-                bestMove: bestMove,
-                bestScore: bestScore,
-                score: null,
-                moveType: null,
-                bestWinChance: getWinChance(bestScore),
-                winChance: null
-              }
-            } else {
-              console.log(dataArray)
-              evaluations[i] = {
-                bestMove: bestMove,
-                bestScore: null,
-                score: null,
-                moveType: null,
-                bestWinChance: null,
-                winChance: null
-              }
-            }
-          }
-          bestMovesCount++
-          if (bestMovesCount === moves.length) {
-            console.log(`Time taken: ${Date.now() - now}ms`)
-            for (let i = 1; i < evaluations.length; i++) {
-              evaluations[i - 1].score = evaluations[i].bestScore
-            }
-            const ret: Evaluation[][] = []
-            for (let i = 0; i < evaluations.length / 2; i++) {
-              const whiteIndex = i * 2
-              const blackIndex = i * 2 + 1
-              evaluations[whiteIndex].winChance = getWinChance(evaluations[whiteIndex].score)
-              evaluations[whiteIndex].moveType = getMoveType(Math.abs(getWinChance(evaluations[whiteIndex].bestScore) - getWinChance(evaluations[whiteIndex].score)))
-              if (evaluations[blackIndex]) {
-                evaluations[blackIndex].winChance = getWinChance(evaluations[blackIndex].score)
-                evaluations[blackIndex].moveType = getMoveType(Math.abs(getWinChance(evaluations[blackIndex].bestScore) - getWinChance(evaluations[blackIndex].score)))
-              }
-              ret.push([evaluations[whiteIndex], evaluations[blackIndex]])
-            }
-            resolve(ret)
-          }
-          stockfish.kill()
-        } else if (data.toString().includes("info depth 15")) {
-          const dataArray = data.toString().split(" ")
-          const isWhite = i % 2 !== 1
+    stockfish.stdout.on("data", (data) => {
+      if (data.includes('bestmove') && moveIndex < moves.length + 1) {
+        const dataArray = data.toString().split(" ")
+        const bestMove = dataArray[dataArray.findIndex((x: string) => x.includes("bestmove")) + 1].substring(0, 4)
+        const isWhite = moveIndex % 2 !== 1
+        if (dataArray.includes("score")) {
           let bestScore
           if (dataArray.includes("mate")) {
             const value = dataArray[dataArray.findIndex((x: string) => x.includes("mate")) + 1]
@@ -132,38 +68,63 @@ export async function POST(request: Request) {
               if (isWhite) bestScore = `M${value}`
               else bestScore = `-M${value}`
             }
-          }
-          else bestScore = (parseInt(dataArray[dataArray.findIndex((x: string) => x.includes("cp")) + 1]) / (isWhite ? 100 : -100)).toString()
-          evaluations[i] = {
-            bestMove: null,
+          } else bestScore = (parseInt(dataArray[dataArray.findIndex((x: string) => x.includes("cp")) + 1]) / (isWhite ? 100 : -100)).toString()
+          evaluations[moveIndex] = {
+            bestMove: bestMove,
             bestScore: bestScore,
             score: null,
             moveType: null,
             bestWinChance: getWinChance(bestScore),
             winChance: null
           }
+        } else console.log("no score")
+        moveIndex++
+        if (evaluations.length === moves.length + 1) {
+          console.log(`Time taken: ${Date.now() - now}ms`)
+          for (let i = 0; i < evaluations.length - 1; i++) {
+            evaluations[i].score = evaluations[i + 1].bestScore
+          }
+          evaluations.pop()
+          const ret: Evaluation[][] = []
+          for (let i = 0; i < evaluations.length / 2; i++) {
+            const whiteIndex = i * 2
+            const blackIndex = i * 2 + 1
+            evaluations[whiteIndex].winChance = getWinChance(evaluations[whiteIndex].score)
+            evaluations[whiteIndex].moveType = getMoveType(Math.abs(getWinChance(evaluations[whiteIndex].bestScore) - getWinChance(evaluations[whiteIndex].score)))
+            if (evaluations[blackIndex]) {
+              evaluations[blackIndex].winChance = getWinChance(evaluations[blackIndex].score)
+              evaluations[blackIndex].moveType = getMoveType(Math.abs(getWinChance(evaluations[blackIndex].bestScore) - getWinChance(evaluations[blackIndex].score)))
+            }
+            ret.push([evaluations[whiteIndex], evaluations[blackIndex]])
+          }
+          resolve(ret)
+        } else if (evaluations.length === moves.length) {
+          stockfish.stdin.write(`position fen ${moves[moves.length - 1].after}\n`)
+          stockfish.stdin.write(`go depth ${depth}\n`)
+        } else {
+          stockfish.stdin.write(`position fen ${moves[moveIndex].before}\n`)
+          stockfish.stdin.write(`go depth ${depth}\n`)
         }
-      })
-
-      stockfish.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`)
-      })
-
-      // Function to send commands to Stockfish
-      const sendCommandToStockfish = (command: string) => {
-        stockfish.stdin.write(command + '\n');
       }
+    })
 
-      if (i === moves.length) sendCommandToStockfish(`position fen ${moves[i - 1].after}`)
-      else sendCommandToStockfish(`position fen ${moves[i].before}`)
-      sendCommandToStockfish(`go depth 15`)
+    stockfish.stderr.on("data", (data) => {
+      console.error(`stderr: ${data}`)
+    })
+
+    stockfish.on("close", (code) => {
+      console.log(`child process exited with code ${code}`)
+    })
+
+    // Function to send commands to Stockfish
+    const sendCommandToStockfish = (command: string) => {
+      stockfish.stdin.write(command + '\n');
     }
+
+    sendCommandToStockfish(`position fen ${moves[0].before}\n`)
+    sendCommandToStockfish(`go depth ${depth}\n`)
   })
 
   const evaluations = await getEvaluations
-  evaluations.pop()
-  for (let i = 0; i < evaluations.length; i++) {
-    console.log(`Move ${i + 1}: ${JSON.stringify(evaluations[i])}`)
-  }
   return Response.json(evaluations)
 }
